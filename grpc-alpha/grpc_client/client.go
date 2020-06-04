1c2e612e3397a22e7f905efb5d1dc3c89a1a0d6e
Evan Gallis <evanrgallis@gmail.com>
2020-06-04 13:50:04 -0400

    added competition function
    simple usage of the oneof struct

--- grpc-alpha/grpc_client/client.go
+++ grpc-alpha/grpc_client/client.go
@@ -42,6 +42,24 @@ func hostTest(c pb.LaforgeClient, name string) {
 /*  BASE LAFORGE */
 // Fields Source: https://app.swaggerhub.com/apis/LaForge/LaforgeAPI/0.0.1-oas3#
 
+// Request Competition - by string name, string id
+func competition(c pb.LaforgeClient, name string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// COMP_NAME/ID are interchangeable due to "oneof" proto definition
+	comp_id := &pb.CompetitionRequest_Id{Id: "1234"}
+	r, err := c.GetCompetition(ctx, &pb.CompetitionRequest{Comp: comp_id})
+
+	//comp_name := &pb.CompetitionRequest_Name{Name: name}
+	//r, err := c.GetCompetition(ctx, &pb.CompetitionRequest{Comp: comp_name})
+
+	if err != nil {
+			log.Fatalf("could not greet: %v", err)
+	}
+	//print server demo response
+	log.Printf("Competition Name: %v | ID: %v | Env: %v | Users: %v | Build Config: %v", r.GetName(), r.GetId(), r.GetEnvironments(), r.GetUsers(), r.GetBuildConfigs())
+}
 
 func main() {
 	// Set up a connection to the server.
@@ -57,12 +75,14 @@ func main() {
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
-
+	
+	// competition
+	comp_name := "Demo Comp"
 
 	// END VARS
 
 	ping(c, name)
 	hostTest(c, name)
-	
+	competition(c, comp_name)
 
 }
--- grpc-alpha/grpc_server/server.go
+++ grpc-alpha/grpc_server/server.go
@@ -31,6 +31,30 @@ func (s *server) GetHostTest(ctx context.Context, in *pb.HostTestRequest) (*pb.H
 	return &pb.HostTestReply{Name: in.GetName(), Id: in.GetId(), Ip: in.GetIp(), Os: in.GetOs()}, nil
 }
 
+/*  BASE LAFORGE */
+// Fields Source: https://app.swaggerhub.com/apis/LaForge/LaforgeAPI/0.0.1-oas3#
+
+//Competition Info
+func (s *server) GetCompetition(ctx context.Context, in *pb.CompetitionRequest) (*pb.CompetitionReply, error) {
+	// set defaults if name or id is omitted due to oneof from client request
+	name := "N/A"
+	id := "000000"
+
+	switch {
+	case in.GetName() != "":
+		name = in.GetName()
+	case in.GetId() != "":
+		id = in.GetId()
+	}
+
+	log.Printf("Client Send - Competition Name: %v | ID: %v", in.GetName(), in.GetId())
+	log.Printf("Server Change - Competition Name: %v | ID: %v", name, id)
+
+	//demo response
+	comp := pb.CompetitionReply{Name: name, Id: id, Environments: []int32{1, 2, 3}, Users: []int32{11, 22, 33}, BuildConfigs: []int32{111, 222, 333}}
+	return &comp, nil
+}
+
 
 func main() {
 	lis, err := net.Listen("tcp", port)
