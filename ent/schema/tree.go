104b8ed48f955c927f2acbc4ebdb137fed1bd72f
Connor Leavesley <cxl6834@g.rit.edu>
2020-10-21 22:41:49 -0400

    Hopefully entc will track the schema if I commit?

---
+++ ent/schema/tree.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
+
+// Tree holds the schema definition for the Tree entity.
+type Tree struct {
+	ent.Schema
+}
+
+// Fields of the Tree.
+func (Tree) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id"),
+	}
+}
+
+// Edges of the Tree.
+func (Tree) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("target", Tree.Type),
+		edge.From("source", Tree.Type),
+	}
+}
